7d25dd281e98c71247e16bc4571ec5147593d04b
Farooq Ansari <farooq.ansari21297@gmail.com>
2020-01-17 00:39:14 +0530

    Running the models

--- ILPD/main.py
+++ ILPD/main.py
@@ -1,15 +1,24 @@
 import csv
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-from sklearn.linear_model import LogisticRegression
-from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn import metrics, preprocessing
 from sklearn.impute import SimpleImputer
+from itertools import combinations
+
+from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier, plot_importance
 from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+
+
 
 dataset = pd.read_csv('dataset.csv')
 
@@ -21,10 +30,43 @@ dataset = pd.DataFrame(imp.transform(dataset))
 X, y = dataset.iloc[:, :-1], dataset.iloc[:, -1]
 X = preprocessing.scale(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/4, random_state = 2)
-model = LogisticRegression(C = 0.1, solver = 'liblinear') # RandomForestClassifier() # SVC() #XGBClassifier() #LogisticRegression(C = 0.1)
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+max_acc = 0
+
+for i in range(1,11): # no. of columns at a time
+
+	for columns in combinations(range(1,10),i): # all combinations of i columns
+
+		columns = list(columns)
+
+		X = pd.DataFrame(X)
+
+		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 2)
 
 
-print("Accuracy: ", float(model.score(X_test, y_test))*100)
+		X_train = X_train[columns]
+		X_test = X_test[columns]
+
+		model = LogisticRegression(C = 1, solver = 'liblinear') # RandomForestClassifier() # SVC() #XGBClassifier() #LogisticRegression(C = 0.1)
+		model.fit(X_train, y_train)
+		#print(model.feature_importances_)
+		y_pred = model.predict(X_test)
+
+		accuracy = round(float((model.score(X_test, y_test)*100)),2)
+
+		if accuracy > max_acc:
+			max_acc = accuracy
+			best_columns = columns
+			print("max till now", max_acc)
+
+
+print("Accuracy: ", max_acc, ' with ', best_columns)
+
+
+# Logistic with C = 0.01 with [2,4,9] has 74.74% accuracy 
+# MLPClassifier with [1,5,7] has 74.23% accuracy
+# RandomForestClassifier with [4,6,8] has 77.32% accuracy
+# RandomForestClassifier Accuracy:  78.35  with  [1, 3, 4, 6, 8]
+# DecisionTreeClassifier Accuracy:  74.23  with  [3, 4, 6, 9]
+# MLPClassifier Accuracy:  75.77  with  [1, 2, 4, 5, 6, 7, 8, 9]
+# Logistic with C = 0.1 Accuracy:  75.26  with  [2, 4, 8, 9]
+# Logistic with C = 1 Accuracy:  75.77  with  [2, 4, 8, 9]
